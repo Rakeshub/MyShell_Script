c9f047b41017208aa11210c3cd540156c4bcf877
BhargavaSai <bsai2020@gmail.com>
2017-04-08 17:11:56 +0530

    Title: New Programs  Details:  Issue no:  Feature_no:

--- MyShell_Script/loop.sh
+++ MyShell_Script/loop.sh
@@ -1,9 +1,10 @@
-#!/bin/bash
-#
+#!/usr/bin/env bash 
+#She bang Line
 for((i=0;i<=10;i++))
 do
  touch test$i.sh
- ifconfig>test$i.sh
- chmod 755 *.*
+ #ifconfig>test$i.sh
+ # chmod 755 *.*
 
 done
+
